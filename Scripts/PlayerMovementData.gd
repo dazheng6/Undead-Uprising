c9463f5d7a5917ebc2c593a10937fb4b645f2e65
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2024-03-01 13:37:44 -0800

    Uploaded Heart Platformer

---
+++ Scripts/PlayerMovementData.gd
@@ -0,0 +1,10 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 100.0
+@export var acceleration = 800.0
+@export var friction = 700.0
+@export var jump_velocity = -300.0
+@export var gravity_scale = 0.6
+@export var air_resistnace = 200.0
+@export var ait_acceleration = 400.0
---
+++ Scripts/player.gd
@@ -0,0 +1,84 @@
+extends CharacterBody2D
+
+@export var movement_data : PlayerMovementData
+
+var air_jump = false
+var just_wall_jumped = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var starting_position = global_position
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_wall_jump()
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
+	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y  >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+	just_wall_jumped = false
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * movement_data.gravity_scale * delta
+
+func handle_wall_jump():
+	if not is_on_wall_only(): return
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.x = wall_normal.x * movement_data.speed * 1.5
+		velocity.y = movement_data.jump_velocity * .6
+		just_wall_jumped = true
+		
+func handle_jump():
+	if is_on_floor(): air_jump = true
+		
+	if is_on_floor() or coyote_jump_timer.time_left < 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = movement_data.jump_velocity * .7
+	if not is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity * .6
+			
+		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
+			velocity.y = movement_data.jump_velocity * .7
+			air_jump = false
+
+func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_resistnace * delta)	
+
+func apply_air_resistance(input_axis,delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistnace * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+
+func _on_hazard_detector_area_entered(area):
+	global_position = starting_position
