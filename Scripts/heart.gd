b05babc55143dcb3c95a5744cf4eb00c7cae9c1b
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2024-03-07 14:01:25 -0800

    Made the bullet destroy the hearts

--- Scripts/heart.gd
+++ Scripts/heart.gd
@@ -2,6 +2,15 @@ extends Area2D
 
 func _on_body_entered(body):
 	queue_free()
+	print("destroy_heart_body")
+	var hearts = get_tree().get_nodes_in_group("Hearts")
+	if hearts.size() == 1:
+		Events.level_completed.emit()
+
+
+func _on_area_entered(area):
+	queue_free()
+	print("destroy_heart")
 	var hearts = get_tree().get_nodes_in_group("Hearts")
 	if hearts.size() == 1:
 		Events.level_completed.emit()
