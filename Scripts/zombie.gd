0f5961a7f6cf9b7aef4dba5d191c2b55c177ea84
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2024-03-08 14:32:03 -0800

    Started on the coding for the zombie
    Broken

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
+var bulletPath = preload("res://Scenes/bullet.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func shoot():
+	var bullet_instance = bulletPath.instantiate()
+	add_child(bullet_instance)
+	rotation_degrees = $Marker2D.rotation_degrees
+	var direction = direction = Vector2.RIGHT.rotated(deg_to_rad(rotation_degrees))
+	bullet_instance.set_velocity(direction)
