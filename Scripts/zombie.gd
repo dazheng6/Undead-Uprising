73cf2e48ad4c29f8b9f95b1832fc82e7a3af0212
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2024-03-11 13:38:47 -0700

    Added Health to the Zombie

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 var bulletPath = preload("res://Scenes/bullet.tscn")
 @onready var zombieHitTime = $Timer
+@onready var zombieHealth = 100.0
 
-func _ready():
-	zombieHitTime.start()
+func _process(delta):
+	if zombieHealth == 0:
+		queue_free()
+		print("Zombie Died")
 
 func shoot():
 	var bullet_instance = bulletPath.instantiate()
@@ -13,6 +16,10 @@ func shoot():
 	var direction = Vector2.RIGHT.rotated(deg_to_rad(rotation_degrees))
 	bullet_instance.set_velocity(-direction)
 
-func _on_timer_timeout():
+func _on_hazard_detector_area_entered(area):
+	print("Zombie Shoot")
 	shoot()
-	print("ZombieHitTime Start")
+
+func _on_area_entered(area):
+	print("Zombie -25 HP")
+	zombieHealth -= 25
