7c46983756c42f100880629e8384d6242be83452
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2024-03-15 13:25:37 -0700

    added player hud and game over screen

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,34 +4,57 @@ extends CharacterBody2D
 @export var bullet : PackedScene
 var bulletPath = preload("res://Scenes/bullet.tscn")
 @onready var playerHealth = 100.0
-@onready var lives = 3
+@export var lives = 3
 var air_jump = false
 var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var was_wall_normal = Vector2.ZERO
+@onready var sprite = $AnimatedSprite2D
+#timers
 @onready var reloadTimer = $ReloadTimer
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
-@onready var starting_position = global_position
 @onready var wall_jump_timer = $WallJumpTimer
+@onready var respawn_timer = $RespawnTimer
+@onready var starting_position = global_position
 
-var current_ammo = 30
+#ui variables
+@onready var ammo_text = $PlayerHUD/AmmoLabel
+@onready var healthbar = $PlayerHUD/HealthBar
+@onready var lives_text = $PlayerHUD/LivesCount
+@onready var death_transistion = $PlayerHUD/DeathTransistion/AnimationPlayer
+@onready var game_over_screen = $PlayerHUD/AnimationPlayer
+
+@export var current_ammo = 30
 var max_ammo = 30
+var is_dead = false
+var is_reloading = false
 
 func _ready():
 	$Marker2D.position = Vector2(15,0)
+	update_ammo_text()
+	update_lives_count()
 
 func _process(delta):
 	if lives == 0:
-		queue_free()
-		print("You Died")
+		game_over_screen.play("text_fade")
+		sprite.visible = false
 	
-	if playerHealth == 0:
-		global_position = starting_position
-		print("Player Died")
-		playerHealth += 100
+	if is_dead == false:
+		move()
+	else:
+		return
+	
+	
+	if playerHealth <= 0:
+		is_dead = true
+		animated_sprite_2d.play("idle")
 		lives -= 1
-		
+		if lives != 0:
+			fade_to_black()
+		respawn_timer.start()
+
+func move():
 	if Input.is_action_just_pressed("move_left"):
 		$Marker2D.position = Vector2(-15,0)
 		$Marker2D.set_rotation(-3.14159)
@@ -40,18 +63,17 @@ func _process(delta):
 		$Marker2D.position = Vector2(15, 0)
 		$Marker2D.set_rotation(0)
 		
-	if Input.is_action_just_pressed("shoot") and current_ammo > 0:
+	if Input.is_action_just_pressed("shoot") and current_ammo > 0 and !is_reloading:
 		print("Shot a bullet")
 		shoot()
-	if Input.is_action_just_pressed("reload") and current_ammo < max_ammo:
+	if Input.is_action_just_pressed("reload") and current_ammo < max_ammo and !is_reloading:
 		reload()
-
 func shoot():
 	var bullet_instance = bulletPath.instantiate()
 	current_ammo -= 1
 	add_child(bullet_instance)
 	bullet_instance.position = $Marker2D.position
-
+	update_ammo_text()
 	# Get the rotation of the marker in degrees
 	var rotation_degrees = $Marker2D.rotation_degrees
 	# Calculate the direction vector based on the rotation
@@ -61,30 +83,53 @@ func shoot():
 
 func reload():
 	reloadTimer.start()
+	is_reloading = true
 	print("reloading")
+	ammo_text.text = ("Reloading...")
+
+func respawn():
+	is_dead = false
+	global_position = starting_position
+	playerHealth += 100
+	update_healthbar()
+	update_lives_count()
+	fade_from_black()
+
+
+func update_ammo_text():
+	ammo_text.text = str(current_ammo) + "/" + str(max_ammo)
+
+func update_lives_count():
+	if lives < 10:
+		lives_text.text = "0" + str(lives - 1)
+	else:
+		lives_text.text = str(lives)
+
 
 func _physics_process(delta):
-	apply_gravity(delta)
-	handle_wall_jump()
-	handle_jump()
-	var input_axis = Input.get_axis("move_left", "move_right")
-	handle_acceleration(input_axis, delta)
-	handle_air_acceleration(input_axis, delta)
-	apply_friction(input_axis, delta)
-	apply_air_resistance(input_axis, delta)
-	update_animations(input_axis)
-	var was_on_floor = is_on_floor()
-	var was_on_wall = is_on_wall_only()
-	if was_on_wall:
-		was_wall_normal = get_wall_normal()
-	move_and_slide()
-	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y  >= 0
-	if just_left_ledge:
-		coyote_jump_timer.start()
-	just_wall_jumped = false
-	var just_left_wall = was_on_wall and not is_on_wall()
-	if just_left_wall:
-		wall_jump_timer.start()
+	if !is_dead :
+		
+		apply_gravity(delta)
+		handle_wall_jump()
+		handle_jump()
+		var input_axis = Input.get_axis("move_left", "move_right")
+		handle_acceleration(input_axis, delta)
+		handle_air_acceleration(input_axis, delta)
+		apply_friction(input_axis, delta)
+		apply_air_resistance(input_axis, delta)
+		update_animations(input_axis)
+		var was_on_floor = is_on_floor()
+		var was_on_wall = is_on_wall_only()
+		if was_on_wall:
+			was_wall_normal = get_wall_normal()
+		move_and_slide()
+		var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y  >= 0
+		if just_left_ledge:
+			coyote_jump_timer.start()
+		just_wall_jumped = false
+		var just_left_wall = was_on_wall and not is_on_wall()
+		if just_left_wall:
+			wall_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -145,8 +190,29 @@ func update_animations(input_axis):
 func _on_hazard_detector_area_entered(area):
 	print("Player Hit")
 	playerHealth -= 50
+	update_healthbar()
 
+func update_healthbar():
+	var tween = get_tree().create_tween()
+	tween.tween_property(healthbar, "value", playerHealth, 0.2)
 func _on_reload_timer_timeout():
 	current_ammo = max_ammo
-	print("reloaded")
+	update_ammo_text()
 	reloadTimer.stop()
+	is_reloading = false
+
+func _on_respawn_timer_timeout():
+	if lives > 0:
+		respawn()
+	elif lives == 0:
+		get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
+	respawn_timer.stop()
+	
+
+func fade_from_black():
+	death_transistion.play("fade_from_black")
+	await death_transistion.animation_finished
+
+func fade_to_black():
+	death_transistion.play("fade_to_black")
+	await death_transistion.animation_finished
--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -4,13 +4,16 @@ var bulletPath = preload("res://Scenes/bullet.tscn")
 @onready var zombieHitTime = $Timer
 @onready var zombieHealth = 100.0
 @onready var zombie = get_tree().get_nodes_in_group("Zombie")
-
+@onready var healthbar = $ProgressBar
 func _process(delta):
 	if zombieHealth == 0:
 		queue_free()
 		print("Zombie Died")
+	update_healthbar()
 
-
+func update_healthbar():
+	var tween = get_tree().create_tween()
+	tween.tween_property(healthbar, "value", zombieHealth, 0.2)
 
 func shoot():
 	var bullet_instance = bulletPath.instantiate()
@@ -24,5 +27,7 @@ func _on_hazard_detector_area_entered(area):
 	shoot()
 
 func _on_area_entered(area):
+	healthbar.visible = true
+	update_healthbar()
 	print("Zombie -25 HP")
 	zombieHealth -= 25
